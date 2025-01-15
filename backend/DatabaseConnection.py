50f8efbc97b0af58e1427a91d21ef0f4f9689b7a
jaynie12 <jaynieshah2000@gmail.com>
2025-01-15 15:42:38 +0000

    Adding the post request to notion

--- backend/DatabaseConnection.py
+++ backend/DatabaseConnection.py
@@ -7,9 +7,9 @@ SECRET_KEY = notionKey.notion_key()
 DATABASE_ID = notionKey.database_id()
 #basic headers to set up the request
 HEADERS =  {'Authorization': f"Bearer {SECRET_KEY}", 
-
            'Content-Type': 'application/json', 
            'Notion-Version': '2022-06-28'}
+
 class DatabaseConnection:
     """
     Ability to connect to notion api
@@ -18,34 +18,78 @@ class DatabaseConnection:
         self.notion_key = secret_key
         self.notion_headers = notion_headers
         self.database_id = database_id
+        self.page_id=None
 
     #finding everything tied to my integration
     def response_search(self):
         json_search_param = {"filter": {"value": "page", "property": "object"}}
         search_response = requests.post(f'https://api.notion.com/v1/search', json=json_search_param, headers=self.notion_headers)
+        self.page_id=search_response['results'][0]["id"]
         return search_response.json()
     
-    #post request
+    
     def create_page(self, data: dict):
         create_url = "https://api.notion.com/v1/pages"
-        # parent = the database where the new page is inserted, represented as a JSON object with a page_id or database_id key, and the corresponding ID.
 
-        payload = {"parent": {"database_id": self.database_id}, "properties": data}
+        #payload = {"parent": {"database_id": DATABASE_ID}, "properties": data}
 
-        res = requests.post(create_url, headers=self.notion_headers, json=payload)
-        # print(res.status_code)
+        res = requests.post(create_url, headers=self.notion_headers, json=data)
+        print(res.text)
         return res
 
-    def query_database(self):
-            db_id = self.database_id
-            url = f"https://api.notion.com/v1/databases/{db_id}/query"
+    def query_database(self, num_pages = None):
+        """
+        If num_pages is None, get all pages, otherwise just the defined number.
+        """
+        url = f"https://api.notion.com/v1/databases/{DATABASE_ID}/query"
+
+        get_all = num_pages is None
+        page_size = 100 if get_all else num_pages
+
+        payload = {"page_size": page_size}
+        response = requests.post(url, json=payload, headers=self.notion_headers)
+
+        data = response.json()
+
+        #Comment this out to dump all data to a file
+        import json
+        with open('db.json', 'w', encoding='utf8') as f:
+            json.dump(data, f, ensure_ascii=False, indent=4)
+
+        results = data["results"]
+        # while data["has_more"] and get_all:
+        #     payload = {"page_size": page_size, "start_cursor": data["next_cursor"]}
+        #     url = f"https://api.notion.com/v1/databases/{DATABASE_ID}/query"
+        #     response = requests.post(url, json=payload, headers=self.notion_headers)
+        #     data = response.json()
+        #     results.extend(data["results"])
+
+        return data
 
 
+#answer = DatabaseConnection(SECRET_KEY,HEADERS, DATABASE_ID).response_search()
 
+payload = {
+    "parent": {
+        "database_id": DATABASE_ID
+    },
+    "properties": {
+                "activity_type": {
+                    "select": {
+                        "name": "Listening",
+                    }
+                },
+                "Description": {
+                    "id": "title",
+                    "type": "title",
+                    "title": [
+                        {
+                            "type": "text",
+                            "text": {
+                                "content": "Fait test"}
+                        }]
+}}}
 
-
-# answer = DatabaseConnection(SECRET_KEY,HEADERS).response_search()
-
-#answer = DatabaseConnection(SECRET_KEY,HEADERS).create_page()
-
-# print(answer)
+#read_db =  DatabaseConnection(SECRET_KEY,HEADERS,DATABASE_ID).query_database()
+create_test = DatabaseConnection(SECRET_KEY,HEADERS,DATABASE_ID).create_page(payload)
+print(create_test)
